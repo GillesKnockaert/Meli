871ae5fb7cd196c5e593113f34ac33b72c86c903
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2016-04-27 16:59:08 +0200

    Added database write on form submit

--- src/BazookasBundle/Controller/DefaultController.php
+++ src/BazookasBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace BazookasBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use BazookasBundle\Form\CollectionItemType;
+use BazookasBundle\Entity\CollectionItem;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -29,14 +30,30 @@ class DefaultController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $item = json_encode($form->getData());
+            $data = $form->getData();
 
-            var_dump($item);
-            exit();
+            $collectionItem = new CollectionItem();
+            $collectionItem->setTitleNL($data["titleNL"]);
+            $collectionItem->setTitleFR($data["titleFR"]);
+            $collectionItem->setDescriptionNL($data["descriptionNL"]);
+            $collectionItem->setDescriptionFR($data["descriptionFR"]);
+            $collectionItem->setImageURL($data["imageURL"]);
+            $collectionItem->setCategoryID($data["categoryID"]->getId());
+            $collectionItem->setYear($data["year"]);
+            $collectionItem->setType($data["type"]);
+            $collectionItem->setMustShowDate($data["mustShowDate"]);
+            $collectionItem->setPositionX($data["positionX"]);
+            $collectionItem->setPositionY($data["positionY"]);
+            $collectionItem->setYearFrom($data["yearFrom"]);
+            $collectionItem->setYearTill($data["yearTill"]);
+            $collectionItem->setColumnID($data["columnID"]);
 
             $em = $this->getDoctrine()->getManager();
-            $em->persist($item);
+            $em->persist($collectionItem);
             $em->flush();
+
+            var_dump('success');
+            exit();
         }
 
         return $this->render('BazookasBundle:Default:index.html.twig', array('form' => $form->createView()));
