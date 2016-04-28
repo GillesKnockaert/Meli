279cc6b027867aaeedd7f02da75894cf2fdc2b31
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2016-04-28 11:38:06 +0200

    CollectionItem: list, edit, remove, add

--- src/BazookasBundle/Controller/DefaultController.php
+++ src/BazookasBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace BazookasBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 use BazookasBundle\Form\CollectionItemType;
 use BazookasBundle\Entity\CollectionItem;
 use BazookasBundle\Form\MediaType;
@@ -17,15 +18,23 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BazookasBundle:Default:index.html.twig');
+        return new Response('Home page');
     }
 
+    /**
+     * @Route("/collectionitem/list")
+     */
+    public function collectionItemListAction() {
+        $repository = $this->getDoctrine()->getRepository('BazookasBundle:CollectionItem');
+        $collectionItems = $repository->findAll();
+
+        return $this->render('BazookasBundle:Default:collectionList.html.twig', array('items' => $collectionItems));
+    }
 
     /**
-     * @Route("/collectionitem")
+     * @Route("/collectionitem/add")
      */
-    public function collectionItemAction(Request $request)
-    {
+    public function collectionItemAddAction(Request $request) {
         $collectionItemType = new CollectionItemType();
         $form = $collectionItemType->buildForm($this->createFormBuilder(), array());
 
@@ -35,26 +44,13 @@ class DefaultController extends Controller
             $data = $form->getData();
 
             $collectionItem = new CollectionItem();
-            $collectionItem->setTitleNL($data["titleNL"]);
-            $collectionItem->setTitleFR($data["titleFR"]);
-            $collectionItem->setDescriptionNL($data["descriptionNL"]);
-            $collectionItem->setDescriptionFR($data["descriptionFR"]);
-            $collectionItem->setImageURL($data["imageURL"]);
-            $collectionItem->setCategoryID($data["categoryID"]->getId());
-            $collectionItem->setYear($data["year"]);
-            $collectionItem->setType($data["type"]);
-            $collectionItem->setMustShowDate($data["mustShowDate"]);
-            $collectionItem->setPositionX($data["positionX"]);
-            $collectionItem->setPositionY($data["positionY"]);
-            $collectionItem->setYearFrom($data["yearFrom"]);
-            $collectionItem->setYearTill($data["yearTill"]);
-            $collectionItem->setColumnID($data["columnID"]);
+            $collectionItem = $this->setCollectionItemProperties($collectionItem, $data);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($collectionItem);
             $em->flush();
 
-            return $this->redirect('media');
+            return $this->redirect('/meli/collectionitem/list');
         }
 
 
@@ -62,14 +58,100 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/media")
+     * @Route("/collectionitem/edit/{id}")
      */
-    public function mediaAction()
-    {
-        $mediaType = new MediaType();
-        $form = $mediaType->buildForm($this->createFormBuilder(), array());
+    public function collectionItemEditAction(Request $request, $id) {
+        $item = $this->getDoctrine()->getRepository('BazookasBundle:CollectionItem')->find($id);
+
+        $collectionItemType = new CollectionItemType();
+        $form = $collectionItemType->buildForm($this->createFormBuilder(), array());
+
+        $form = $this->fillCollectionItemForm($form, $item);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $item = $this->setCollectionItemProperties($item, $data);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirect('/meli/collectionitem/list');
+        }
+
 
         return $this->render('BazookasBundle:Default:index.html.twig', array('form' => $form->createView()));
     }
 
+    /**
+     * @Route("/collectionitem/delete/{id}")
+     */
+    public function collectionItemDeleteAction($id) {
+        $item = $this->getDoctrine()->getRepository('BazookasBundle:CollectionItem')->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($item);
+        $em->flush();
+
+        return $this->redirect('/meli/collectionitem/list');
+    }
+
+    /**
+     * @Route("/media/list")
+     */
+    public function mediaListAction() {
+
+    }
+
+    public function mediaAddAction() {
+
+    }
+
+    public function mediaEditAction() {
+
+    }
+
+    public function mediaDeleteAction() {
+
+    }
+
+    private function setCollectionItemProperties($item, $data) {
+        $item->setTitleNL($data["titleNL"]);
+        $item->setTitleFR($data["titleFR"]);
+        $item->setDescriptionNL($data["descriptionNL"]);
+        $item->setDescriptionFR($data["descriptionFR"]);
+        $item->setImageURL($data["imageURL"]);
+        $item->setCategoryID($data["categoryID"]->getId());
+        $item->setYear($data["year"]);
+        $item->setType($data["type"]);
+        $item->setMustShowDate($data["mustShowDate"]);
+        $item->setPositionX($data["positionX"]);
+        $item->setPositionY($data["positionY"]);
+        $item->setYearFrom($data["yearFrom"]);
+        $item->setYearTill($data["yearTill"]);
+        $item->setColumnID($data["columnID"]);
+
+        return $item;
+    }
+
+    private function fillCollectionItemForm($form, $item) {
+        $form->get('titleNL')->setData($item->getTitleNL());
+        $form->get('titleFR')->setData($item->getTitleFR());
+        $form->get('descriptionNL')->setData($item->getDescriptionNL());
+        $form->get('descriptionFR')->setData($item->getDescriptionFR());
+        $form->get('imageURL')->setData($item->getImageURL());
+        $form->get('categoryID')->setData($item->getCategoryID());
+        $form->get('year')->setData($item->getYear());
+        $form->get('type')->setData($item->getType());
+        $form->get('mustShowDate')->setData($item->getMustShowDate());
+        $form->get('positionX')->setData($item->getPositionX());
+        $form->get('positionY')->setData($item->getPositionY());
+        $form->get('yearFrom')->setData($item->getYearFrom());
+        $form->get('yearTill')->setData($item->getYearTill());
+        $form->get('columnID')->setData($item->getColumnID());
+
+        return $form;
+    }
 }
